5835a484d624153a83b9558048867e65f1bf79b4
Admin <admin@MacBook-Pro-Admin.local>
2020-04-26 11:13:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const wrapper = document.querySelector('.wrapper');
+
+function createCard(cardObj) {
+    return `
+<div class="card">
+    <div class="header">
+        <img class="header-image" src="https://cheatsnah.com/wp-content/uploads/2015/12/super-mario-run-vzlom-cheats.jpeg" alt="avatar">
+        <div class="header-title">
+            ${cardObj.author}   
+        </div>
+    </div>
+    <div class="image-wrapper">
+        <img class="image" src="${cardObj.download_url}" alt="image test">
+    </div>
+    <div class="content">
+        Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+    </div>
+</div> 
+`;
+}
+
+fetch("https://picsum.photos/v2/list")
+    .then(function(response) {
+        return response.json();
+    })
+    .then((data) => {
+        let cardList = '';
+        data.forEach((cardObj) => {
+           cardList = cardList + createCard(cardObj) 
+        })
+
+        wrapper.innerHTML = cardList;
+
+        let msnry;
+        setTimeout(() => {
+            msnry = new Masonry(wrapper);
+        }, 1000)
+
+        setTimeout(() => {
+            msnry.reloadItems();
+        }, 2000)
+    });
